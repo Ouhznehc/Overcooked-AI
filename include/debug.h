ed512f8353af3c0c69ca54f119e0a9d441e764d6
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-04-26 12:29:36 +0800

    compile

--- include/debug.h
+++ include/debug.h
@@ -5,27 +5,36 @@
 
 #define __DEBUG_MODE__
 
-
-#ifdef assert
-#undef assert
-#endif
-#ifdef panic
-#undef panic
-#endif
-#ifdef panic_on
-#undef panic_on
-#endif
-
 #ifdef __DEBUG_MODE__
 
-// extern FILE* debug_log;
 #define __FILENAME__ (strrchr(__FILE__, '/') ? (strrchr(__FILE__, '/') + 1):__FILE__)
 
 #define Log(format,...) \
-  fprintf(stderr, "[%s, %d, %s] " format "\n", \
-      __FILENAME__, __LINE__, __func__, ## __VA_ARGS__)
+      fprintf(stderr, "[%s, %d, %s] " format "\n", \
+          __FILENAME__, __LINE__, __func__, ## __VA_ARGS__)
+
+#define panic(format, ...) \
+      do { \
+        Log("panic: " format, ## __VA_ARGS__); \
+        _halt(1); \
+      } while (0)
+
+#define Assert(cond,format,...) \
+      do{ \
+        if(!(cond)) { \
+          panic(format); \
+        }  \
+      }while(0)
+
+#define TODO() panic("please implement me")
 
 
+#else
+
+#define Log(format,...)
+#define panic(format, ...)
+#define Assert(cond,format,...)
+#define TODO()
 #endif
 
 #endif
