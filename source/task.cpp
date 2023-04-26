551901add2b0e707c227856f3ca9ec7dcbc7db0e
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-04-26 16:46:25 +0800

    compile

--- source/task.cpp
+++ source/task.cpp
@@ -1,6 +1,6 @@
 #include <task.h>
 
 std::string allocate_task(Player player) {
-  cerr << "allocate_task:" << std::endl;
+  std::cerr << "allocate_task:" << std::endl;
   return std::string("Move R");
 }
