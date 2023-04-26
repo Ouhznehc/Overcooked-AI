086b997fee03e151a94e988f87022fb734e873d8
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-04-26 15:18:01 +0800

    compile

---
+++ source/common.cpp
@@ -0,0 +1,18 @@
+const int N = 20 + 5;
+
+/* 按照读入顺序定义 */
+int width, height;
+char Map[N][N];
+int IngredientCount;
+struct Ingredient Ingredient[N];
+int recipeCount;
+struct Recipe Recipe[N];
+int totalTime, randomizeSeed, totalOrderCount;
+struct Order totalOrder[N];
+int orderCount;
+struct Order Order[N];
+int k;
+struct Player Players[2 + 5];
+int entityCount;
+struct Entity Entity[N];
+int remainFrame, Fund;
