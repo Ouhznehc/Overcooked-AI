08342f28ace547f89d4e1c773a3a24e6b8354dd3
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-04-26 12:23:21 +0800

    compile

--- include/debug.h
+++ include/debug.h
@@ -19,10 +19,11 @@
 #ifdef __DEBUG_MODE__
 
 // extern FILE* debug_log;
+#define __FILENAME__ (strrchr(__FILE__, '/') ? (strrchr(__FILE__, '/') + 1):__FILE__)
 
 #define Log(format,...) \
   fprintf(stderr, "[%s,%d,%s] " format "\n", \
-      basename(__FILE__), __LINE__, __func__, ## __VA_ARGS__)
+      __FILENAME__, __LINE__, __func__, ## __VA_ARGS__)
 
 
 #endif
