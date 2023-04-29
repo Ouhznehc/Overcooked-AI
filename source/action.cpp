d942d3865877c286b08a638eea6908843e8e74e4
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-04-29 22:41:41 +0800

    compile

--- source/action.cpp
+++ source/action.cpp
@@ -54,7 +54,7 @@ std::pair<bool, std::string> interact(Player player, std::string object) {
 
 }
 std::pair<bool, std::string> put_or_pick(Player player, std::string object) {
-  if (player.container == Container::Plate) return { false, "" };
+  if (player.container == Container::Plate && object == "Plate") return { false, "" };
   for (int i = 0; i < player.entity.size(); i++) {
     if (player.entity[i] == object) return { false, "" };
   }
