16436634e020549caed717cba3c38df3b1100b9f
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-04-29 20:04:19 +0800

    compile

--- source/action.cpp
+++ source/action.cpp
@@ -1,6 +1,8 @@
 #include <action.h>
 #include <common.h>
 
+#define LIMIT_RANGE 0.1
+
 double manhattan_distance(location src, location dst) {
   double distance = std::fabs(src.x - dst.x) + std::fabs(src.y - dst.y);
   return distance;
@@ -8,18 +10,18 @@ double manhattan_distance(location src, location dst) {
 
 bool check_arive(location src, location dst) {
   bool flag1 = manhattan_distance(src, dst) <= 0.8;
-  bool flag2 = std::fabs(src.x - dst.x) <= 0.01 || std::fabs(src.y - dst.y) <= 0.01;
+  bool flag2 = std::fabs(src.x - dst.x) <= LIMIT_RANGE || std::fabs(src.y - dst.y) <= LIMIT_RANGE;
   return flag1 && flag2;
 }
 
 std::string move_towards_by_location(location src, location dst) {
   double delta_x = dst.x - src.x, delta_y = dst.y - src.y;
   int x_flag, y_flag;
-  if (std::fabs(delta_x) <= 0.1) x_flag = 0;
-  else if (delta_x > 0.1) x_flag = 1;
+  if (std::fabs(delta_x) <= LIMIT_RANGE) x_flag = 0;
+  else if (delta_x > LIMIT_RANGE) x_flag = 1;
   else x_flag = -1;
-  if (std::fabs(delta_y) <= 0.1) y_flag = 0;
-  else if (delta_y > 0.1) y_flag = 1;
+  if (std::fabs(delta_y) <= LIMIT_RANGE) y_flag = 0;
+  else if (delta_y > LIMIT_RANGE) y_flag = 1;
   else y_flag = -1;
   if (x_flag == 1 && y_flag == 1) return "RD";
   if (x_flag == -1 && y_flag == 1) return "LD";
