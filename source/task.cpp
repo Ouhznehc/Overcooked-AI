f1abac104db4e2962241f513405e50cf2f73dd84
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-04-29 22:28:59 +0800

    compile

--- source/task.cpp
+++ source/task.cpp
@@ -2,12 +2,12 @@
 #include <action.h>
 #include <common.h>
 #include <debug.h>
-
 std::deque<task> task_pool;
 task taskPool[7] = { {schedule_move, "fish"}, {schedule_put_or_pick, "fish"}, {schedule_move, "Plate"}, {schedule_put_or_pick, "Plate"}, {schedule_put_or_pick, "Plate"}, {schedule_move, "service_window"}, {schedule_put_or_pick, "service_window"} };
 task* current_task;
 int current;
 std::pair<std::string, std::string> allocate_task() {
+  Log("current = %d \n", current);
   if (current_task == NULL) current_task = &taskPool[current];
   auto rc = current_task->function(Players[0], current_task->object);
   if (rc == "") current_task = &taskPool[(current + 1) % 7], current++;
