e12d0cbcd9f0fbd5247b4540760733b6fa4bc0e4
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-04-29 23:02:11 +0800

    compile

--- source/task.cpp
+++ source/task.cpp
@@ -3,7 +3,7 @@
 #include <common.h>
 #include <debug.h>
 std::deque<task> task_pool;
-task player0_task_pool[7] = { {schedule_move, "rice"}, {schedule_put_or_pick, "rice"}, {schedule_move, "Plate"}, {schedule_put_or_pick, "Plate"}, {schedule_put_or_pick, "Plate"}, {schedule_move, "service_window"}, {schedule_put_or_pick, "service_window"} };
+task player0_task_pool[7] = { {schedule_move, "fish"}, {schedule_put_or_pick, "fish"}, {schedule_move, "Plate"}, {schedule_put_or_pick, "Plate"}, {schedule_put_or_pick, "Plate"}, {schedule_move, "service_window"}, {schedule_put_or_pick, "service_window"} };
 task* player0_task;
 int player0_current;
 std::pair<std::string, std::string> allocate_task() {
