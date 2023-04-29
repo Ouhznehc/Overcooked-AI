eb1e263639a1ab236dbdf8d259db57aa56d1bda9
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-04-29 20:18:08 +0800

    compile

--- source/task.cpp
+++ source/task.cpp
@@ -10,7 +10,7 @@ std::pair<std::string, std::string> allocate_task() {
   return { schedule_move(Player[0], "Plate"), schedule_move(Player[1], "fish") };
 }
 
-std::string schedule_move(Player, player, std::string dest) {
+std::string schedule_move(Player player, std::string dest) {
   auto rc = move_towards(player, dest);
   return "Move " + rc.second;
 }
