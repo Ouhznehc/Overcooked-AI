066cfad521c4ebde588f510f53de49c6ab95493e
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-04-29 20:19:40 +0800

    compile

--- include/task.h
+++ include/task.h
@@ -14,7 +14,7 @@ extern std::deque<task> task_pool;
 
 void update_task_pool();
 void schedule_task();
-std::string schedule_move(std::string dest);
+std::string schedule_move(Player player, std::string dest);
 std::pair<std::string, std::string> allocate_task();
 
 #endif
