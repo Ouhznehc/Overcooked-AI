e43888276044b5bf69007e74e849cfb6def64905
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-04-29 19:57:51 +0800

    compile

--- source/action.cpp
+++ source/action.cpp
@@ -9,10 +9,21 @@ double manhattan_distance(location src, location dst) {
 
 std::string move_towards_by_location(location src, location dst) {
   double delta_x = dst.x - src.x, delta_y = dst.y - src.y;
-  if (delta_x > 0.1) return "R";
-  if (delta_x < -0.1) return "L";
-  if (delta_y > 0.1) return "D";
-  if (delta_y < -0.1) return "U";
+  int x_flag, y_flag;
+  if (std::fabs(delta_x) <= 0.1) x_flag = 0;
+  else if (delta_x > 0.1) x_flag = 1;
+  else x_flag = -1;
+  if (std::fabs(delta_y) <= 0.1) y_flag = 0;
+  else if (delta_y > 0.1) y_flag = 1;
+  else y_flag = -1;
+  if (x_flag == 1 && y_flag == 1) return "RD";
+  if (x_flag == -1 && y_flag == 1) return "LD";
+  if (x_flag == 1 && y_flag == -1) return "RU";
+  if (x_flag == -1 && y_flag == -1) return "LU";
+  if (x_flag == 0 && y_flag == 1) return "D";
+  if (x_flag == 0 && y_flag == -1) return "U";
+  if (x_flag == 1 && y_flag == 0) return "R";
+  if (x_flag == -1 && y_flag == 0) return "L";
   return " ";
 }
 
