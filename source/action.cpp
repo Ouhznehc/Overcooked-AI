60317f9cfb3b76081d01d42db852463e5795231d
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-04-29 22:22:59 +0800

    compile

--- source/action.cpp
+++ source/action.cpp
@@ -47,6 +47,7 @@ std::pair<bool, std::string> move_towards(Player player, std::string dest) {
 }
 
 std::pair<bool, std::string> interact(Player player, std::string object) {
+  return { false, "" };
 
 }
 std::pair<bool, std::string> put_or_pick(Player player, std::string object) {
