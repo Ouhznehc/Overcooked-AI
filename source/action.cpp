73c5e305f78d09a3cfe1543e37b84b234cad2b45
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-01 01:11:50 +0800

    compile

--- source/action.cpp
+++ source/action.cpp
@@ -3,7 +3,7 @@
 #include <bits/stdc++.h>
 
 #define LIMIT_RANGE 0.3
-#define SAFE_DISTANCE 2.02
+#define SAFE_DISTANCE 2.03
 #define DELTA_X(src, dst) (dst.x - src.x)
 #define DELTA_Y(src, dst) (dst.y - src.y)
 
