529b3c230ab172107cc1e43dfc15956de74b771f
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-01 00:38:18 +0800

    compile

--- source/task.cpp
+++ source/task.cpp
@@ -39,7 +39,7 @@ std::pair<bool, std::string> schedule_interact(Player player, std::string dest) 
 }
 
 std::pair<bool, std::string> schedule_stop(Player player, std::string dest) {
-  std::cerr << "loop " << remainFrame << std::endl;
   if (player.x_velocity == 0 && player.y_velocity == 0) return { false, "Move  " };
+  std::cerr << "loop " << remainFrame << std::endl;
   return { true, "Move  " };
 }
