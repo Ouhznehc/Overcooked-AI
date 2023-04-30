1e653a32eae7a46c5c7c800faa094c919cd43f92
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-04-30 22:19:51 +0800

    compile

--- source/action.cpp
+++ source/action.cpp
@@ -73,7 +73,17 @@ std::pair<bool, std::string> move_and_put_or_pick(Player player, std::string des
 
 
 std::pair<bool, std::string> interact(Player player, std::string object) {
-  return { false, "" };
+  location dst_location = set_dest_location(*LUT.find(object)->second.begin());
+  for (int i = 0; i < entityCount; i++) {
+    if (Entity[i].sum != 0) {
+      if (dst_location.x == 1.5) return { true, "L" };
+      if (dst_location.x == width - 1.5) return { true, "R" };
+      if (dst_location.y == 1.5) return { true, "U" };
+      if (dst_location.y == height - 1.5) return { true, "D" };
+      break;
+    }
+  }
+  return { false, " " };
 }
 
 
--- source/task.cpp
+++ source/task.cpp
@@ -4,18 +4,25 @@
 #include <debug.h>
 std::deque<task> task_pool;
 task player0_task_pool[4] = { {schedule_move_and_put_or_pick, "fish"} , {schedule_move_and_put_or_pick, "Plate"}, {schedule_move_and_put_or_pick, "Plate"}, {schedule_move_and_put_or_pick, "service_window"} };
-// task player1_task_pool[7] = { {schedule_move, "dirty_plate_location"}, {schedule_put_or_pick, "dirty_plate_location"}, {schedule_move, "sink"}, {schedule_put_or_pick, "sink"}, {schedule_put_or_pick, "Plate"}, {schedule_move, "service_window"}, {schedule_put_or_pick, "service_window"} };
+task player1_task_pool[3] = { {schedule_move_and_put_or_pick, "dirty_plate_location"}, {schedule_move_and_put_or_pick, "sink"}, {schedule_interact, "DirtyPlates"} };
 task* player0_task;
+task* palyer1_task;
 int player0_current;
+int player1_current;
 std::pair<std::string, std::string> allocate_task() {
   if (player0_task == NULL) player0_task = &player0_task_pool[player0_current];
+  if (player1_task == NULL) player1_task = &player1_task_pool[player1_current];
   auto rc0 = player0_task->function(Players[0], player0_task->object);
+  auto rc1 = player1_task->function(Players[1], palyer1_task->object);
   if (rc0.first == false) {
     player0_current = (player0_current + 1) % 4;
     player0_task = &player0_task_pool[player0_current];
-    return { rc0.second, "" };
   }
-  else return { rc0.second, "" };
+  if (rc1.first == false) {
+    player1_current = (player1_current + 1) % 3;
+    player1_task = &player1_task_pool[player1_current];
+  }
+  return { rc0.second, rc1.second };
 }
 
 std::pair<bool, std::string> schedule_move_and_put_or_pick(Player player, std::string dest) {
@@ -25,5 +32,5 @@ std::pair<bool, std::string> schedule_move_and_put_or_pick(Player player, std::s
 
 std::pair<bool, std::string> schedule_interact(Player player, std::string dest) {
   auto rc = interact(player, dest);
-  return { rc.first, rc.first ? "Interact " + rc.second : "" };
+  return { rc.first, rc.first ? "Interact " + rc.second : "Move " + rc.second };
 }
