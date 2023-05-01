c2acc6af168de59a5af937cfc3c0ab9ef7057252
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-01 18:37:49 +0800

    compile

--- source/action.cpp
+++ source/action.cpp
@@ -194,6 +194,14 @@ std::priority_queue <Node> spfa_pq;
 std::map <int_loc, int> dist;
 
 int get_shortest_path(int_loc from, int_loc dest) {
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      fprintf(stderr, "%d ", ValueMap[i][j]);
+    }
+    fprintf(stderr, "\n");
+  }
+
   dist.clear();
   while (!spfa_pq.empty()) spfa_pq.pop();
 
@@ -206,8 +214,8 @@ int get_shortest_path(int_loc from, int_loc dest) {
     if (now_node.score > dist[now_node.pos])
       continue;
     int_loc now_pos = now_node.pos;
-    fputs("HERE\n", stderr);
-    fprintf(stderr, "%d %d %d\n", now_pos.x, now_pos.y, now_pos.get_value());
+    // fputs("HERE\n", stderr);
+    // fprintf(stderr, "%d %d %d\n", now_pos.x, now_pos.y, now_pos.get_value());
     for (int direction = 0; direction < Direction_N; direction++) {
       if (!now_pos[direction].isvalid())
         continue;
