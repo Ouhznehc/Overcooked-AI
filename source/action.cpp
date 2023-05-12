6953afc6decf37a3761889db872c141c4b270cf2
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-12 14:41:28 +0800

    compile

--- source/action.cpp
+++ source/action.cpp
@@ -343,11 +343,14 @@ std::pair<bool, std::string> move_and_put_or_pick(Player player, std::string des
 std::pair<bool, std::string> interact(Player player, std::string object) {
   static int flag = 0;
   for (int i = 0; i < N; i++) {
-    // std::cerr << Entity[i].entity[1] << std::endl;
     if (Entity[i].container == Container::None) {
+      std::cerr << "None" << std:endl;
+      std::cerr << Entity[i].entity[0] << std::endl;
       if (Entity[i].entity[0] != object) continue;
     }
     else {
+      std::cerr << "FUCK" << std::endl;
+      std::cerr << Entity[i].entity[0] << std::endl;
       if (Entity[i].entity[1] != object) continue;
     }
     if ((Entity[i].total_frame != 0 || !flag)) {
