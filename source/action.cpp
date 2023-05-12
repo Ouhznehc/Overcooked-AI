df0629f9f6dc0fbb61bfe8df78eb2484af95a96e
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-12 15:32:38 +0800

    compile

--- source/action.cpp
+++ source/action.cpp
@@ -345,6 +345,7 @@ std::pair<bool, std::string> interact(Player player, std::string object) {
   for (int i = 0; i < N; i++) {
     if (Entity[i].container == Container::None) {
       if (object == "fish") {
+        std::cerr << "===========" << std::endl;
         std::cerr << "None" << std::endl;
         std::cerr << Entity[i].entity[0] << std::endl;
       }
