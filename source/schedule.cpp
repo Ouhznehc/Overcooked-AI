30e1d211e8b990db09fe188720656134f19a3e0b
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-20 22:09:27 +0800

    compile

--- source/schedule.cpp
+++ source/schedule.cpp
@@ -28,8 +28,9 @@ static std::pair<Location, Location> fetch_task_dst() {
   if (static_lut.find(cook_object) != static_lut.end()) cook_dst = static_lut.at(cook_object)[0];
   else if (dynamic_lut.find(cook_object) != dynamic_lut.end()) cook_dst = dynamic_lut.at(cook_object)[0];
   else {
-    std::cerr << "cannot find dst" << std::endl;;
-    assert(0);
+    cook_dst = static_lut.at("sink")[0];
+    // std::cerr << "cannot find dst" << std::endl;;
+    // assert(0);
   }
   if (static_lut.find(wash_object) != static_lut.end()) wash_dst = static_lut.at(wash_object)[0];
   else if (dynamic_lut.find(wash_object) != dynamic_lut.end()) wash_dst = dynamic_lut.at(wash_object)[0];
