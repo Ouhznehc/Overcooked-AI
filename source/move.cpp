5e7a6e603bcf8ad70f80196d590a0e48c69539f5
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-20 17:01:08 +0800

    compile

--- source/move.cpp
+++ source/move.cpp
@@ -1,9 +1,9 @@
 #include <move.h>
 
 std::pair<bool, std::string> move_towards_by_location(Location src, Location dst, int id) {
-  return { true, "Move L" };
+  return { true, "Move R" };
 }
 
 std::pair<bool, std::string> lazy_around_dog(Location src, Location dst, int id) {
-  return { true, "Move R" };
+  return { true, "Move L" };
 }
