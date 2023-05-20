8a56539f82715f2f7f29bd6ec5adbb7254c20616
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-20 21:35:10 +0800

    compile

--- source/move.cpp
+++ source/move.cpp
@@ -12,14 +12,34 @@ static Location fetch_move_dst(Location dst) {
   assert(0);
 }
 
+static bool is_arive(Location src, Location dst, int id) {
+  double hamilton_distance = std::fabs(src.x - dst.x) + std::fabs(src.y - dst.y);
+  double hamilton_velocity = std::fabs(player[id].x_velocity) + std::fabs(player[id].y_velocity);
+  return hamilton_distance < 0.05 && hamilton_velocity < 0.01;
+}
+
+static std::string fetch_pick_direction(Location src, Location dst) {
+  if (dst.x == 0) return "L";
+  if (dst.x == width - 1) return "R";
+  if (dst.y == 0) return "U";
+  if (dst.y == height - 1) "D";
+  assert(0);
+}
+
 std::pair<bool, std::string> move_towards_by_location(Location src, Location dst, int id) {
   std::string move_direction = "";
+  std::string pick_direction = "";
   Location move_src = fetch_move_src(src);
   Location move_dst = fetch_move_dst(dst);
+  if (is_arive(move_src, move_dst, id)) {
+    pick_direction = fetch_pick_direction(move_src, move_dst);
+    return { false, "PutOrPick " + pick_direction };
+  }
   if (move_src.x > move_dst.x) move_direction += "L";
   if (move_src.x < move_dst.x) move_direction += "R";
   if (move_src.y > move_dst.y) move_direction += "U";
   if (move_src.y < move_dst.y) move_direction += "D";
+
   return { true, "Move " + move_direction };
 }
 
