405bb77303aa6464f8298c366097f0906da642ef
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-21 19:23:58 +0800

    compile

--- source/IO_handle.cpp
+++ source/IO_handle.cpp
@@ -205,5 +205,5 @@ void init() {
     for (int i = 0; i < IngredientCount; i++) {
         LUT[Ingredient[i].name].push_back({ (double)Ingredient[i].x, (double)Ingredient[i].y });
     }
-    LUT["temp"].push_back({ 0, 1 });
+    LUT["temp"].push_back({ 9, 2 });
 }
