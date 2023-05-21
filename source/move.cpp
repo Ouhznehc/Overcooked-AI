fe534a4f287bcf08a8c7571b9015ced93cc0f944
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-21 12:04:27 +0800

    compile

--- source/move.cpp
+++ source/move.cpp
@@ -13,9 +13,11 @@ static Location fetch_move_dst(Location dst) {
 }
 
 static bool is_arive(Location src, Location dst, int id) {
-  double hamilton_distance = std::fabs(src.x - dst.x) + std::fabs(src.y - dst.y);
-  double hamilton_velocity = std::fabs(player[id].x_velocity) + std::fabs(player[id].y_velocity);
-  return hamilton_distance < 0.1;
+  dst.x = dst.x + 0.5; dst.y = dst.y + 0.5;
+  double delta_x_2 = std::fabs(src.x - dst.x) * std::fabs(src.x - dst.x);
+  double delta_y_2 = std::fabs(src.y - dst.y) * std::fabs(src.y - dst.y);
+  double distance = std::sqrt(delta_x_2 + delta_y_2);
+  return distance < 1.35;
 }
 
 static std::string fetch_pick_direction(Location src, Location dst) {
@@ -31,7 +33,7 @@ std::pair<bool, std::string> move_towards_by_location(Location src, Location dst
   std::string pick_direction = "";
   Location move_src = fetch_move_src(src);
   Location move_dst = fetch_move_dst(dst);
-  if (is_arive(move_src, move_dst, id)) {
+  if (is_arive(move_src, dst, id)) {
     pick_direction = fetch_pick_direction(move_src, dst);
     return { false, "PutOrPick " + pick_direction };
   }
