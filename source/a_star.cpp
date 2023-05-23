86cbed051a633d239c36f6f444748b2775393376
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-23 18:50:09 +0800

    compile

--- source/a_star.cpp
+++ source/a_star.cpp
@@ -53,11 +53,11 @@ bool is_obstacle(int id, int direction, Point point) {
 
 
 
-  if (point.x + dx[direction] == player_point.x && point.y == player_point.y) return true;
-  if (point.x == player_point.x && point.y + dy[direction] == player_point.y) return true;
-  if (point.x + dx[direction] == player_point.x && point.y + dy[direction] == player_point.y) return true;
+  // if (point.x + dx[direction] == player_point.x && point.y == player_point.y) return true;
+  // if (point.x == player_point.x && point.y + dy[direction] == player_point.y) return true;
+  // if (point.x + dx[direction] == player_point.x && point.y + dy[direction] == player_point.y) return true;
 
-  if (player[id ^ 1].move_direction == " ") return false;
+  // if (player[id ^ 1].move_direction == " ") return false;
 
   int player_direction = -1;
   for (int i = 0; i < 8; i++) if (player[id ^ 1].move_direction == Direction[i]) player_direction = i;
