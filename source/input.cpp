c83ed8e369975267009b24e38e8cdbfddd66c95c
tracer-ps4 <tracer-ps4@nju.edu.cn>
2023-05-23 18:37:01 +0800

    compile

--- source/input.cpp
+++ source/input.cpp
@@ -159,7 +159,9 @@ static void update_static_lut() {
             case 'c': static_lut["Chop"].push_back(Location(i, j)); break;
             case 'k': static_lut["sink"].push_back(Location(i, j)); break;
             case 'r': static_lut["clean_plate_location"].push_back(Location(i, j)); break;
-            case 'p': static_lut["dirty_plate_location"].push_back(Location(i, j)); break;
+            case 'p': static_lut["dirty_plate_location"].push_back(Location(i, j));
+                static_lut["plate_location"].push_back(Location(i, j - 1));
+                break;
             case '$': static_lut["service_window"].push_back(Location(i, j)); break;
             default:
                 break;
@@ -175,7 +177,6 @@ static void update_static_lut() {
         }
         if (entity[i].item[0] == "Pan") {
             static_lut["pan_location"].push_back(entity[i].location);
-            static_lut["plate_location"].push_back({ entity[i].location.x, entity[i].location.y + 1 });
         }
     }
 }
